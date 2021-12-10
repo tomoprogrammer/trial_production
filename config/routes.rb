abc0b873243cee3d2b87f7b2c91bcff121e665c7
tomoprogrammer <train.vegacat@gmail.com>
2021-12-10 17:34:47 +0000

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
+  resources :moneys, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
