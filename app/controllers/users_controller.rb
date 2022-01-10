5b72c7f99c84c0062e973e6b4b17d9da768b1ea5
tomoprogrammer <train.vegacat@gmail.com>
2022-01-10 14:50:40 +0000

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
   
   def index
+    @money = Money.new
+    @user = current_user
+    @users = User.all
   end
   
   def show
+    @user = User.find(params[:id])
+    @moneys = @user.moneys
+    @money = Money.new
   end
   
   def edit
+    @user = User.find(params[:id])
+    render :edit
   end
   
   def update
+    @user = User.find(params[:id])
+    redirect_to user_path(@user.id)
   end
   
-  def destroy
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
   
 end
