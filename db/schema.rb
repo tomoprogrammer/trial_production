215fd8eeaa93793ba4484baf49081a860b974f38
tomoprogrammer <train.vegacat@gmail.com>
2022-01-25 04:40:29 +0000

    register

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2022_01_05_081325) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
